2f4b58aade4c9e4289ea56d9a4bd4b709ddba9df
Nico Alexander <nico.alxr@gmail.com>
2023-01-26 17:14:49 +0100

    Add arrow for direction

--- src/index.py
+++ src/index.py
@@ -24,6 +24,8 @@ class Sim:
         self.canvas_ctx = canvas_ctx
 
         self.keyboardInputHandler = KeyboardInputHandler()
+        self.keyboardInputHandler.on_key_down(lambda k: f"Down {k}")
+        self.keyboardInputHandler.on_key_up(lambda k: f"Up {k}")
         self.robot = Robot(canvas_ctx)
 
     def update(self) -> None:
@@ -52,11 +54,11 @@ class Robot:
         self.canvas_ctx = canvas_ctx
         self.pixels_per_meter = pixels_per_meter
 
-        self.heading = 0
+        self.heading = math.pi / 4
         self.speed = 0
         self.x = x_in_pixels / self.pixels_per_meter if x_in_pixels else self.WHEEL_BASE / 2
         self.y = y_in_pixels / self.pixels_per_meter if y_in_pixels else self.LENGTH / 2
-        self.rot = 5 * math.pi / 3
+        self.rot = 0
 
     def draw(self) -> None:
         self.draw_rect(
@@ -65,17 +67,9 @@ class Robot:
             0
         )
 
-        self.draw_rect(
-            -self.WHEEL_BASE / 2, -self.ROD_THICKNESS / 2 - self.LENGTH / 2,
-            self.WHEEL_BASE, self.ROD_THICKNESS,
-            0
-        )
-
-        self.draw_rect(
-            -self.WHEEL_BASE / 2, -self.ROD_THICKNESS / 2 + self.LENGTH / 2,
-            self.WHEEL_BASE, self.ROD_THICKNESS,
-            0
-        )
+        self.draw_axel(-self.LENGTH / 2, 0)
+        self.draw_axel(self.LENGTH / 2, self.heading)
+        self.draw_arrow((self.LENGTH / 2) + 0.05, self.heading)
 
     def draw_rect(
             self,
@@ -115,6 +109,85 @@ class Robot:
         self.canvas_ctx.strokeStyle = "green"
         self.canvas_ctx.stroke()
 
+    def draw_triangle_around_center(
+            self,
+            x: float,
+            y: float,
+            width: float,
+            height: float,
+            rel_rot: float,
+    ) -> None:
+        x_min = x
+        y_min = y
+        x_max = x + width
+        x_mid = x_max / 2
+        y_max = y + height
+
+        c = ((x_min + x_max) / 2, (y_min + y_max) / 2)
+
+        p1 = (x_min, y_min)
+        p2 = (x_max, y_min)
+        p3 = (x_mid, y_max)
+
+        self.canvas_ctx.beginPath()
+        is_first_point = True
+        for p in (p1, p2, p3):
+            canvas_p = rotate_around_point(c[0], c[1], p[0], p[1], rel_rot)
+            canvas_p = translate(canvas_p[0], canvas_p[1], self.x, self.y)
+            canvas_p = rotate_around_point(self.x, self.y, canvas_p[0], canvas_p[1], self.rot)
+            canvas_p = scale_from_origin(canvas_p[0], canvas_p[1], self.pixels_per_meter)
+
+            if is_first_point:
+                self.canvas_ctx.moveTo(canvas_p[0], canvas_p[1])
+                is_first_point = False
+            else:
+                self.canvas_ctx.lineTo(canvas_p[0], canvas_p[1])
+
+        self.canvas_ctx.closePath()
+        self.canvas_ctx.strokeStyle = "green"
+        self.canvas_ctx.stroke()
+
+    def draw_rect_around_center(
+            self,
+            x: float,
+            y: float,
+            width: float,
+            height: float,
+            rel_rot: float,
+    ) -> None:
+        self.draw_rect(x - width / 2, y - height / 2, width, height, rel_rot)
+
+    def draw_axel(self, offset: float, heading: float) -> None:
+        self.draw_rect(
+            -self.WHEEL_BASE / 2, -self.ROD_THICKNESS / 2 + offset,
+            self.WHEEL_BASE, self.ROD_THICKNESS,
+            0
+        )
+
+        self.draw_wheel(-self.WHEEL_BASE / 2, offset, heading)
+        self.draw_wheel(self.WHEEL_BASE / 2, offset, heading)
+
+    def draw_wheel(self, x: float, y: float, heading: float) -> None:
+        self.draw_rect(
+            x - self.WHEEL_WIDTH / 2,
+            y - self.WHEEL_DIAMETER / 2,
+            self.WHEEL_WIDTH,
+            self.WHEEL_DIAMETER,
+            heading,
+        )
+
+    def draw_arrow(self, offset: float, heading: float) -> None:
+        self.draw_rect_around_center(
+            0, self.ROD_THICKNESS + offset,
+            self.WHEEL_BASE / 2, self.ROD_THICKNESS / 2,
+            heading + math.pi / 2
+        )
+        self.draw_triangle_around_center(
+            0, self.ROD_THICKNESS + offset,
+            self.ROD_THICKNESS * 2, self.LENGTH / 6,
+            heading
+        )
+
 
 class KeyboardInputHandler:
 
@@ -122,105 +195,24 @@ class KeyboardInputHandler:
         self.on_key_down_callbacks: list[Callable[[str], None]] = []
         self.on_key_up_callbacks: list[Callable[[str], None]] = []
 
-        document.addEventListener("keydown", create_proxy(self.handle_key_down))
-        document.addEventListener("keyup", create_proxy(self.handle_key_up))
+        main_el = document.getElementsByTagName("main")[0]
+        print(main_el)
+        main_el.addEventListener("keydown", self.handle_key_down)
+        main_el.addEventListener("keyup", self.handle_key_up)
 
-    def on_forward_key_down(self, callback: Callable[[str], None] | Callable[[], None]) -> None:
-        self.on_key_down(callback, "w", "ArrowUp", ignore_case=True)
-
-    def on_forward_key_up(self, callback: Callable[[str], None] | Callable[[], None]) -> None:
-        self.on_key_up(callback, "w", "ArrowUp", ignore_case=True)
-
-    def on_backward_key_down(self, callback: Callable[[str], None] | Callable[[], None]) -> None:
-        self.on_key_down(callback, "s", "ArrowDown", ignore_case=True)
-
-    def on_backward_key_up(self, callback: Callable[[str], None] | Callable[[], None]) -> None:
-        self.on_key_up(callback, "s", "ArrowDown", ignore_case=True)
-
-    def on_left_key_down(self, callback: Callable[[str], None] | Callable[[], None]) -> None:
-        self.on_key_down(callback, "a", "ArrowLeft", ignore_case=True)
-
-    def on_left_key_up(self, callback: Callable[[str], None] | Callable[[], None]) -> None:
-        self.on_key_up(callback, "a", "ArrowLeft", ignore_case=True)
-
-    def on_right_key_down(self, callback: Callable[[str], None] | Callable[[], None]) -> None:
-        self.on_key_down(callback, "d", "ArrowRight", ignore_case=True)
-
-    def on_right_key_up(self, callback: Callable[[str], None] | Callable[[], None]) -> None:
-        self.on_key_up(callback, "d", "ArrowRight", ignore_case=True)
-
-    def on_key_down(
-            self,
-            callback: Callable[[str], None] | Callable[[], None],
-            *expected_keys: str,
-            ignore_case: bool = False
-    ) -> None:
-        self.add_key_callback_to_list(
-            self.on_key_down_callbacks,
-            callback,
-            *expected_keys,
-            ignore_case=ignore_case,
-        )
-
-    def on_key_up(
-            self,
-            callback: Callable[[str], None] | Callable[[], None],
-            *expected_keys: str,
-            ignore_case: bool = False
-    ) -> None:
-        self.add_key_callback_to_list(
-            self.on_key_up_callbacks,
-            callback,
-            *expected_keys,
-            ignore_case=ignore_case,
-        )
+    def on_key_down(self, callback: Callable[[str], None]) -> None:
+        self.on_key_down_callbacks.append(callback)
 
     def handle_key_down(self, event) -> None:
         for callback in self.on_key_down_callbacks:
-            callback(event.key)
+            callback(key)
+
+    def on_key_up(self, callback: Callable[[str], None]) -> None:
+        self.on_key_up_callbacks.append(callback)
 
     def handle_key_up(self, event) -> None:
         for callback in self.on_key_up_callbacks:
-            callback(event.key)
-
-    def add_key_callback_to_list(
-            self,
-            callbacks: list[Callable[[str], None]],
-            callback: Callable[[str], None] | Callable[[], None],
-            *expected_keys: str,
-            ignore_case: bool = False
-    ) -> None:
-
-        def try_callback_with_and_without_key_arg(key: str) -> None:
-            try:
-                return callback(key)
-            except TypeError as err:
-                if "arguments" in str(err):
-                    return callback()
-
-                raise err
-
-        if expected_keys:
-
-            if ignore_case:
-                expected_keys_set = {key.lower() for key in expected_keys}
-            else:
-                expected_keys_set = set(expected_keys)
-
-            def real_callback(key: str) -> None:
-
-                if ignore_case:
-                    key = key.lower()
-
-                if key in expected_keys_set:
-                    try_callback_with_and_without_key_arg(key)
-
-        else:
-
-            def real_callback(key: str) -> None:
-                try_callback_with_and_without_key_arg(key)
-
-        callbacks.append(real_callback)
+            callback(key)
 
 
 def translate(x: float, y: float, x_offset: float, y_offset: float) -> tuple[float, float]:
