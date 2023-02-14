9c2c278816874bbdd94a066e7682f94cdb75a22c
0scarB <oscarb@protonmail.com>
2023-02-14 04:18:32 +0100

    Implement affine transformations

--- calculation/geometry.py
+++ calculation/geometry.py
@@ -3,13 +3,142 @@ from __future__ import annotations
 import abc
 import math
 from abc import ABC
+from collections.abc import Iterable
 from dataclasses import dataclass
 from typing import Any, overload, Iterator, Literal, NoReturn, Protocol
 from typing_extensions import Self
 
 
+_TranslationDirection = Literal[
+    "left-then-forward" , "<^",
+    "left-then-down"    , "<v",
+    "right-then-forward", ">^",
+    "right-then-down"   , ">v",
+]
+
+
+_RotationDirection = Literal[
+    "clockwise"       , "<-.",
+    "counterclockwise", ".->",
+]
+
+_ScaleDirection = Literal[
+    "enlarge", "<>",
+    "shrink" , "><"
+]
+
+
+def vector(x: float, y: float) -> Vector2d:
+    return Vector2d(x, y)
+
+
+@overload
+def transform(vector: Vector2d, transformation: Transformation2d) -> Vector2d: ...
+
+
+@overload
+def transform(x: float, y: float, transformation: Transformation2d) -> Vector2d: ...
+
+
+def transform(*args: float | Vector2d | Transformation2d) -> Vector2d:
+    *args, transformation = args
+    return transformation.__apply_transform_to__(*args)
+
+
+@overload
+def undo_transform(vector: Vector2d, transformation: Transformation2d) -> Vector2d: ...
+
+
+@overload
+def undo_transform(x: float, y: float, transformation: Transformation2d) -> Vector2d: ...
+
+
+def undo_transform(*args: float | Vector2d | Transformation2d) -> Vector2d:
+    *args, transformation = args
+    return transformation.__remove_transform_from__(*args)
+
+
+@overload
+def translate(vector: Vector2d) -> AffineTransformation: ...
+
+
+@overload
+def translate(vector: Vector2d, direction: _TranslationDirection) -> AffineTransformation: ...
+
+
+@overload
+def translate(x: float, y: float) -> AffineTransformation: ...
+
+
+@overload
+def translate(x: float, y: float, direction: _TranslationDirection) -> AffineTransformation: ...
+
+
+def translate(*args: float | Vector2d | _TranslationDirection) -> AffineTransformation:
+    return AffineTransformation().translate(*args)
+
+
+@overload
+def rotate(angle: float) -> AffineTransformation: ...
+
+
+@overload
+def rotate(angle: float, direction: _RotationDirection) -> AffineTransformation: ...
+
+
+def rotate(*args: float | _RotationDirection) -> AffineTransformation:
+    return AffineTransformation().rotate(*args)
+
+
+@overload
+def scale(vector: Vector2d) -> AffineTransformation: ...
+
+
+@overload
+def scale(vector: Vector2d, direction: _ScaleDirection) -> AffineTransformation: ...
+
+
+@overload
+def scale(x: float, y: float) -> AffineTransformation: ...
+
+
+@overload
+def scale(x: float, y: float, direction: _ScaleDirection) -> AffineTransformation: ...
+
+
+@overload
+def scale(x_and_y: float) -> AffineTransformation: ...
+
+
+@overload
+def scale(x_and_y: float, direction: _ScaleDirection) -> AffineTransformation: ...
+
+
+def scale(*args: float | Vector2d | _ScaleDirection) -> AffineTransformation:
+    return AffineTransformation().scale(*args)
+
+
+class Transformation2d(Protocol):
+
+    @overload
+    def __apply_transform_to__(self, vector: Vector2d) -> Vector2d: ...
+
+    @overload
+    def __apply_transform_to__(self, x: float, y: float) -> Vector2d: ...
+
+    def __apply_transform_to__(self, *args: float | Vector2d) -> Vector2d: ...
+
+    @overload
+    def __remove_transform_from__(self, vector: Vector2d) -> Vector2d: ...
+
+    @overload
+    def __remove_transform_from__(self, x: float, y: float) -> Vector2d: ...
+
+    def __remove_transform_from__(self, *args: float | Vector2d) -> Vector2d: ...
+
+
 @dataclass(init=False)
-class AffineTransform:
+class AffineTransformation(Transformation2d):
     matrix: Matrix3x3
 
     @classmethod
@@ -27,14 +156,150 @@ class AffineTransform:
             (0, 0, 1),
         )
 
-    def translate(self, translation: Vector2d) -> None:
-        self._from_matrix(self.matrix * Matrix3x3(
-            (1, 0, translation.x),
-            (0, 1, translation.y),
+    def transform(self, transformation: Self) -> Self:
+        return self._from_matrix(transformation.matrix * self.matrix)
+
+    @overload
+    def translate(self, vector: Vector2d) -> Self: ...
+
+    @overload
+    def translate(self, vector: Vector2d, direction: _TranslationDirection) -> Self: ...
+
+    @overload
+    def translate(self, x: float, y: float) -> Self: ...
+
+    @overload
+    def translate(self, x: float, y: float, direction: _TranslationDirection) -> Self: ...
+
+    def translate(self, *args: float | Vector2d | _TranslationDirection) -> Self:
+        if type(args[-1]) is str:
+            *args, direction = args
+        else:
+            direction = "right-then-up"
+
+        if len(args) == 1:
+            translation_vector = args[0]
+            x, y = translation_vector.x, translation_vector.y
+        else:
+            x, y = args
+
+        if direction == "left-then-up" or direction == "<^":
+            return self.translate(-x, y)
+
+        if direction == "left-then-down" or direction == "<v":
+            return self.translate(-x, -y)
+
+        if direction == "right-then-down" or direction == ">v":
+            return self.translate(x, -y)
+
+        return self._from_matrix(Matrix3x3(
+            (1, 0, x),
+            (0, 1, y),
+            (0, 0, 1)
+        ) * self.matrix)
+
+    @overload
+    def rotate(self, angle: float) -> Self: ...
+
+    @overload
+    def rotate(self, angle: float, direction: _RotationDirection) -> Self: ...
+
+    def rotate(self, *args) -> Self:
+        if type(args[-1]) is str:
+            *args, direction = args
+        else:
+            direction = "counterclockwise"
+
+        angle = args[0]
+
+        if direction == "clockwise" or direction == ".->":
+            return self.rotate(-angle)
+
+        return self._from_matrix(Matrix3x3(
+            (math.cos(angle), -math.sin(angle), 0),
+            (math.sin(angle),  math.cos(angle), 0),
+            (0              ,  0              , 1),
+        ) * self.matrix)
+
+    @overload
+    def scale(self, vector: Vector2d) -> Self: ...
+
+    @overload
+    def scale(self, vector: Vector2d, direction: _ScaleDirection) -> Self: ...
+
+    @overload
+    def scale(self, x: float, y: float) -> Self: ...
+
+    @overload
+    def scale(self, x: float, y: float, direction: _ScaleDirection) -> Self: ...
+
+    @overload
+    def scale(self, x_and_y: float) -> Self: ...
+
+    @overload
+    def scale(self, x_and_y: float, direction: _ScaleDirection) -> Self: ...
+
+    def scale(self, *args: float | Vector2d | _ScaleDirection) -> Self:
+        if type(args[-1]) is str:
+            *args, direction = args
+        else:
+            direction: _ScaleDirection = "enlarge"
+
+        if len(args) == 1:
+            arg = args[0]
+
+            if isinstance(arg, Vector2d):
+                x, y = arg.x, arg.y
+            else:
+                x = y = arg
+        else:
+            x, y = args
+
+        if direction == "shrink" or direction == "><":
+            x, y = 1 / x, 1 / y
+
+        return self._from_matrix(Matrix3x3(
+            (x, 0, 0),
+            (0, y, 0),
             (0, 0, 1),
+        ) * self.matrix)
+
+    def invert(self) -> Self:
+        return self._from_matrix(self.matrix ** -1)
+
+    def __apply_transform_to__(self, *args: float | Vector2d) -> Vector2d:
+        if len(args) == 1:
+            vector = args[0]
+
+            x, y = vector.x, vector.y
+        else:
+            x, y = args
+
+        vec3 = Vector3d(x, y, 1)
+
+        transformed_vec3 = self.matrix * vec3
+
+        return Vector2d(
+            transformed_vec3.x,
+            transformed_vec3.y,
         )
 
-    def rotate(self, angle: float) -> None:
+    def __remove_transform_from__(self, *args: float | Vector2d) -> Vector2d:
+        if len(args) == 1:
+            vector = args[0]
+
+            x, y = vector.x, vector.y
+        else:
+            x, y = args
+
+        vec3 = Vector3d(x, y, 1)
+
+        transformed_vec3 = self.matrix**-1 * vec3
+
+        return Vector2d(
+            transformed_vec3.x,
+            transformed_vec3.y,
+        )
 
 
 class SupportsApproxEquals(Protocol):
@@ -468,8 +733,8 @@ class Matrix3x3(_BaseMatrix):
     def _multiply_by_vector(self, vector: Vector3d) -> Vector3d:
         return Vector3d(
             self[0][0] * vector[0] + self[0][1] * vector[1] + self[0][2] * vector[2],
-            self[1][0] * vector[1] + self[1][1] * vector[1] + self[1][2] * vector[2],
-            self[2][0] * vector[2] + self[2][1] * vector[1] + self[2][2] * vector[2],
+            self[1][0] * vector[0] + self[1][1] * vector[1] + self[1][2] * vector[2],
+            self[2][0] * vector[0] + self[2][1] * vector[1] + self[2][2] * vector[2],
         )
 
     def _multiply_by_matrix(self, matrix: Self) -> Self:
@@ -499,13 +764,35 @@ class Matrix3x3(_BaseMatrix):
         )
 
 
+@overload
+def approx_equals(a: SupportsApproxEquals | float, b: SupportsApproxEquals | float, /, threshold: float) -> bool: ...
+
+
+@overload
+def approx_equals(items: Iterable[SupportsApproxEquals | float], /, threshold: float) -> bool: ...
+
+
 def approx_equals(
-        a: float | SupportsApproxEquals,
-        b: float | SupportsApproxEquals,
-        /,
-        threshold: float
+        *args: SupportsApproxEquals | float | Iterable[SupportsApproxEquals | float] | float,
+        **kwargs: float,
 ) -> bool:
     try:
-        return a.__approx_equals__(b, threshold)
-    except AttributeError:
-        return a - threshold <= b <= a + threshold
+        threshold = kwargs["threshold"]
+    except KeyError:
+        *args, threshold = args
+
+    if len(args) == 2:
+        a, b = args
+        return approx_equals((a, b), threshold)
+
+    args = list(*args)
+    for i, a in enumerate(args[:-1]):
+        for b in args[i + 1:]:
+            try:
+                if not a.__approx_equals__(b, threshold):
+                    return False
+            except AttributeError:
+                if not a - threshold <= b <= a + threshold:
+                    return False
+
+    return True
